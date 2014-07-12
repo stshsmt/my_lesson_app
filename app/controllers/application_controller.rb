d47f4e99e1513386987d6f4dfbe12ed875b16a98
Satoshi Hashimoto <8xx4xx8xx4@gmail.com>
2014-07-13 00:42:59 +0900

    fixed error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+	class Forbidden < ActionController::ActionControllerError; end
+
+	include ErrorHandlers if Rails.env.production?
 end
